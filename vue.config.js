8d17fd00556f0866e08566030956b0f776b163cf
SuperSonic <supersonic@livemail.tw>
2022-05-29 09:19:23 +0800

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+    pages: {
+        index: {
+            title: 'caOS Platform',
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+        }
+    },
+    transpileDependencies: true
 })
