d0c0f39138e3ccdebe56103af867202100723980
SuperSonic <supersonic@livemail.tw>
2022-05-29 10:33:41 +0800

    Update

---
+++ src/views/IndexView.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card>
+    <v-card-title>caOS</v-card-title>
+    <v-card-subtitle>
+      The friendly IoT User Interface Platform.
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "IndexView",
+};
+</script>
